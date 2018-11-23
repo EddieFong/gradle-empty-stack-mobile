895c17d94b9a634fc7471579f0e97e2bbdac3c62
Eddie Fong <fonged@oocl.com>
2018-11-23 11:46:45 +0800

    comment helloworld

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,8 @@ public class App {
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+//        System.out.println(new App().getGreeting());
+
         Mobile mobile1 = new Mobile ( "mobile1name","mobile1color","mobile1brand");
         mobile1.makeCall("mobile1.makeCall.message");
         mobile1.getBasicInfo();
